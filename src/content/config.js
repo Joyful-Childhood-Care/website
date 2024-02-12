258af82247b9b53b63e1e55249b6d694df475773
David Waring <dev@davidwaring.net>
2024-02-11 22:11:51 -0500

    Collections: define from CMS config

--- src/content/config.js
+++ src/content/config.js
@@ -1,22 +1,30 @@
 import { defineCollection, z } from 'astro:content';
+import cms_collections from '../../public/admin/collections.json';
 
-const projects = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    draft: z.boolean()
-  })
+// Base Astro Collections
+const rtn = {};
+
+// Parse each of the CMS collections
+cms_collections.forEach((c) => {
+
+  // Define the schema based on the collection fields
+  const s = {};
+  c.fields.forEach((f) => {
+    let n = f.name;
+    let w = f.widget;
+    let v;
+    if ( w === 'string' ) v = z.string();
+    if ( w === 'datetime' ) v = z.string();
+    if ( w === 'boolean' ) v = z.boolean();
+    if ( v && n !== 'body' ) s[n] = v;
+  });
+
+  // Define the Astro collection
+  rtn[c.name] = defineCollection({
+    type: 'content',
+    schema: z.object(s)
+  });
+
 });
 
-const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    author: z.string(),
-    date: z.string(),
-    draft: z.boolean()
-  })
-});
-
-export const collections = { projects, blog };
+export const collections = rtn;
